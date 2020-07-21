eb7bd7a79cd98d9eebc09188dab018469e9222a3
tarunsrivastava145 <tarun.srivastava@lmh.ox.ac.uk>
2020-07-22 07:30:56 +0800

    adding generate_table.py

--- generate_table.py
+++ generate_table.py
@@ -13,7 +13,7 @@ from scipy.signal import peak_prominences
 warnings.filterwarnings('ignore')
 
 '''
-Initialise script parameters
+INTITALISE SCRIPT PARAMETERS
 '''
 
 DISTANCE = 21  # Distance between peaks
@@ -49,15 +49,21 @@ mobilities=['transit_stations','residential','workplace','parks','retail_recreat
 
 sql_command = """SELECT * FROM mobility WHERE source = %(source)s AND adm_area_1 is NULL"""
 raw_mobility = pd.read_sql(sql_command, conn, params={'source': source})
-raw_mobility = raw_mobility[['countrycode','country','date','transit_stations','residential','workplace','parks','retail_recreation','grocery_pharmacy']]
+raw_mobility = raw_mobility[['countrycode','country','date','transit_stations','residential',
+                             'workplace','parks','retail_recreation','grocery_pharmacy']]
 raw_mobility = raw_mobility.sort_values(by=['countrycode','date']).reset_index(drop=True)
 ### Check no conflicting values for each country and date
 assert not raw_mobility[['countrycode','country','date']].duplicated().any()
 
 # GET GOVERNMENT RESPONSE TABLE
+flags=['stringency_index','c1_school_closing','c2_workplace_closing','c3_cancel_public_events',
+       'c4_restrictions_on_gatherings','c5_close_public_transport','c6_stay_at_home_requirements',
+       'c7_restrictions_on_internal_movement','c8_international_travel_controls']
+
 sql_command = """SELECT * FROM government_response"""
 raw_government_response = pd.read_sql(sql_command, conn)
 raw_government_response = raw_government_response.sort_values(by=['countrycode','date']).reset_index(drop=True)
+raw_government_response = raw_government_response[['countrycode','country','date']+flags]
 ### Check no conflicting values for each country and date
 assert not raw_government_response[['countrycode','country','date']].duplicated().any()
 
@@ -66,36 +72,64 @@ assert not raw_government_response[['countrycode','country','date']].duplicated(
 PRE-PROCESSING for the main tables ['epidemiology','mobility','government_response']
 Steps taken:
 1) Filling gaps in epidemiological data through interpolation √
-2) Filling gaps in mobility data through interpolation 
-3) Filling gaps in government_response data through backfill 
-5) Removing countries that are not in all three tables
+2) Filling gaps in mobility data through interpolation √
+3) Filling gaps in government_response data through backfill √
+5) Removing countries that are not in all three tables √
 6) Computing new cases per day for epidemiological data √
 7) Replace negative/invalid new_cases_per_day with previous valid value √
 '''
 
+##EPIDEMIOLOGY PROCESSING LOOP
 countries = raw_epidemiology['countrycode'].unique()
 epidemiology = pd.DataFrame(columns=['countrycode','country','date','confirmed','new_per_day'])
-
-##EPIDEMIOLOGY PROCESSING LOOP
 for country in countries:
-    data=raw_epidemiology[raw_epidemiology['countrycode']==country].set_index('date')
-    data=data.reindex([x.date() for x in pd.date_range(data.index.values[0],data.index.values[-1])])
-    data[['countrycode','country']]=data[['countrycode','country']].fillna(method='backfill')
-    data['confirmed']=data['confirmed'].interpolate(method='linear')
-    data['new_per_day']=data['confirmed'].diff()
+    data = raw_epidemiology[raw_epidemiology['countrycode']==country].set_index('date')
+    data = data.reindex([x.date() for x in pd.date_range(data.index.values[0],data.index.values[-1])])
+    data[['countrycode','country']] = data[['countrycode','country']].fillna(method='backfill')
+    data['confirmed'] = data['confirmed'].interpolate(method='linear')
+    data['new_per_day'] = data['confirmed'].diff()
     data.reset_index(inplace=True)
-    data['new_per_day'].iloc[np.array(data[data['new_per_day']<0].index)]=\
+    data['new_per_day'].iloc[np.array(data[data['new_per_day']<0].index)] = \
         data['new_per_day'].iloc[np.array(epidemiology[epidemiology['new_per_day']<0].index)-1]
-    data['new_per_day']=data['new_per_day'].fillna(method='ffill')
-    epidemiology = pd.concat((epidemiology,data))
+    data['new_per_day'] = data['new_per_day'].fillna(method='bfill')
+    epidemiology = pd.concat((epidemiology,data)).reset_index(drop=True)
     continue
 
 ##MOBILITY PROCESSING LOOP
+countries = raw_mobility['countrycode'].unique()
+mobility = pd.DataFrame(columns=['countrycode','country','date','transit_stations','residential',
+                                     'workplace','parks','retail_recreation','grocery_pharmacy'])
 for country in countries:
+    data = raw_mobility[raw_mobility['countrycode']==country].set_index('date')
+    data = data.reindex([x.date() for x in pd.date_range(data.index.values[0],data.index.values[-1])])
+    data[['countrycode','country']] = data[['countrycode','country']].fillna(method='backfill')
+    data[mobilities] = data[mobilities].interpolate(method='linear')
+    data.reset_index(inplace=True)
+    mobility = pd.concat((mobility,data)).reset_index(drop=True)
     continue
 
-"""exclude=[]
+##GOVERNMENT_RESPONSE LOOP
+countries = raw_government_response['countrycode'].unique()
+government_response = pd.DataFrame(columns=['countrycode','country','date']+flags)
+
 for country in countries:
-    if len(epidemiology[epidemiology['countrycode']==country])==0 or \
-            len(epidemiology[epidemiology['countrycode'] == country]) == 0
-"""
+    data = raw_government_response[raw_government_response['countrycode']==country].set_index('date')
+    data = data.reindex([x.date() for x in pd.date_range(data.index.values[0],data.index.values[-1])])
+    data[['countrycode','country']] = data[['countrycode','country']].fillna(method='backfill')
+    data[flags] = data[flags].fillna(method='ffill')
+    data.reset_index(inplace=True)
+    government_response = pd.concat((government_response,data)).reset_index(drop=True)
+    continue
+
+##EXCLUSION LOOP (DATA FOR EACH COUNTRY MUST BE AVAILABLE IN ALL THREE TABLES)
+exclude=[]
+for country in countries:
+    if len(epidemiology[epidemiology['countrycode'] == country])==0 or \
+            len(mobility[mobility['countrycode'] == country]) == 0 or \
+        len(government_response[government_response['countrycode'] == country]) == 0:
+        exclude.append(country)
+
+epidemiology = epidemiology[~epidemiology['countrycode'].isin(exclude)]
+mobility = mobility[~mobility['countrycode'].isin(exclude)]
+government_response = government_response[~government_response['countrycode'].isin(exclude)]
+
